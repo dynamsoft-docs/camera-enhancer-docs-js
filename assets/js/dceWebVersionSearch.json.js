99ffcc7364c110305640b6d52924d3fdbef5adb5
Justin <caoguangah@gmail.com>
2023-12-07 16:32:12 +0800

    add version search

---
+++ assets/js/dceWebVersionSearch.json.js
@@ -0,0 +1,28 @@
+[
+    {
+        "version": "4.0.0",
+        "matchList": {
+            "js":{
+                "dcvRepoWeb": [
+                    {
+                        "path": "/programming/javascript/api-reference/license",
+                        "version": "3.0.10"
+                    }, {
+                        "path": "/programming/javascript/api-reference/core",
+                        "version": "3.0.10"
+                    }
+                ],
+                "ddn": "2.0.21"
+            }
+        }
+    },{
+        "version": "3.3.9",
+        "matchList": {
+            "js": {
+                "dbr": "9.6.32",
+                "ddn": "1.0.12",
+                "dlr": "2.2.31"
+            }
+        }
+    }
+]
